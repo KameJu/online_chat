6bc5e0ff5106d24c5450309b366485c1ef6ccadc
Nikita <sasiskasssss6052@gmail.com>
2021-10-18 18:03:25 +0300

    Initial commit: add git support

---
+++ src/main/java/my_projects/online_chat/OnlineChatApplication.java
@@ -0,0 +1,13 @@
+package my_projects.online_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my_projects/online_chat/OnlineChatApplicationTests.java
@@ -0,0 +1,13 @@
+package my_projects.online_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
